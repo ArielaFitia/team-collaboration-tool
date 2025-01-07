6c53f4825cdcbb65a91c92aec8645c60a133b517
Ariela <arielafitiajoharini@gmail.com>
2025-01-07 21:49:03 +0300

    Add basic Swagger/OpenAPI documentation

--- src/main/java/com/ditto/teamcollaborationtool/config/AppConfig.java
+++ src/main/java/com/ditto/teamcollaborationtool/config/AppConfig.java
@@ -1,5 +1,7 @@
 package com.ditto.teamcollaborationtool.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,5 +11,14 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Team Collaboration Tool")
+                        .version("1.0")
+                        .description("A REST API to manage teams and projects. Teams can have multiple members, and each team can manage multiple projects with tasks assigned to specific members."));
     }
 }
